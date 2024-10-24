3a7f40f93d5e535333be278cb4a88fdbaf6ff1e5
Thibault Huyghe <thibault.huyghe@inetum-realdolmen.world>
2024-10-24 12:45:06 +0200

    Realisations page start

---
+++ Models/Realisation.cs
@@ -0,0 +1,24 @@
+﻿namespace AHD.Models
+{
+    public class Realisation
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string MainImageUrl { get; set; }
+
+        public List<string> ImageUrls { get; set; }
+
+        public Realisation(int id, string name, string description, string mainImageUrl, List<string> imageUrls)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            MainImageUrl = mainImageUrl;
+            ImageUrls = imageUrls;
+        }
+    }
+}
---
+++ Pages/Realisations.razor.cs
@@ -0,0 +1,26 @@
+﻿using AHD.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace AHD.Pages
+{
+    public partial class Realisations
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        static string img = "https://www.dakwerken-adviseur.be/wp-content/uploads/2017/10/dakwerken-1.jpg";
+        private List<Realisation> RealisationsList = new List<Realisation>
+        {
+            new Realisation(1, "Test", "Test description", img, [img]),
+            new Realisation(2, "Test", "Test description", img, [img]),
+            new Realisation(3, "Test", "Test description", img, [img]),
+            new Realisation(4, "Test", "Test description", img, [img]),
+            new Realisation(5, "Test", "Test description", img, [img])
+        };
+
+        private void NavigateToDetail(int id)
+        {
+            NavigationManager.NavigateTo($"/realisations/{id}");
+        }
+    }
+}
